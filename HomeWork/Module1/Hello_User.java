7311d27bbedba9edbb0aea3a59d744576f10f29d
S. Levchenko <levatelie@gmail.com>
2017-05-07 11:05:15 +0300

    Hmwrk1.2

--- HomeWork/Module1/Hello_User.java
+++ HomeWork/Module1/Hello_User.java
@@ -6,8 +6,7 @@ public class Hello_User {
     public static void main(String[] args) {
         System.out.println("Enter your name:");
         Scanner sc = new Scanner(System.in);
-        String s1;
-        s1 = sc.nextLine();
+        String s1 = sc.nextLine();
         System.out.println("Hello, " + s1);
 
     }
